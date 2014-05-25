294dced1e92d0aa1be8d8e0657c46e93f0078440
Micah Arvey <micaharvey@gmail.com>
2014-05-25 15:10:06 -0700

    Initial Rails Setup

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Gottalovemusictheory::Application.config.secret_key_base = 'c48e5d08ca420fd2f5d56d48aa8e2fa59b3c826205c632db9120ef23e4282bb12472fd514dda3daa4925d733436ad0731d422c68c245d1d30d0c7c077700f6eb'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Gottalovemusictheory::Application.config.session_store :cookie_store, key: '_gottalovemusictheory_session'
